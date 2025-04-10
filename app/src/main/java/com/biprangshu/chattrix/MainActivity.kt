07c9652d09a826ce4a93c9b4e9f78e8844186cbf
Biprangshu Das <dasbiprangshu@gmail.com>
2025-04-10 20:57:36 +0530

    Tested Lottie animation, works

--- app/src/main/java/com/biprangshu/chattrix/MainActivity.kt
+++ app/src/main/java/com/biprangshu/chattrix/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.biprangshu.chattrix.onboarding.LoginScreen
 import com.biprangshu.chattrix.ui.theme.ChattrixTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChattrixTheme {
-
+                LoginScreen()
             }
         }
     }
--- app/src/main/java/com/biprangshu/chattrix/onboarding/LoginScreen.kt
+++ app/src/main/java/com/biprangshu/chattrix/onboarding/LoginScreen.kt
@@ -1,15 +1,29 @@
 package com.biprangshu.chattrix.onboarding
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimatable
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.biprangshu.chattrix.R
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
-    Surface {
-        Column {
-
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.chatanimation_1))
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().statusBarsPadding().navigationBarsPadding()
+        ) {
+            LottieAnimation(composition = composition)
         }
     }
 }
