78f689885290704587f915563c5b3161e5ec22b2
Biprangshu Das <dasbiprangshu@gmail.com>
2025-04-16 19:22:45 +0530

    Created App wide navigation host (Not Complete)

---
+++ app/src/main/java/com/biprangshu/chattrix/ChattrixNavigation.kt
@@ -0,0 +1,44 @@
+package com.biprangshu.chattrix
+
+import android.app.Activity
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.biprangshu.chattrix.authentication.AuthViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+
+@Composable
+fun ChattrixNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewModel = viewModel(), navController: NavHostController) {
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                account.idToken?.let { token ->
+                    authViewModel.signInWithGoogle(token)
+                }
+            } catch (e: ApiException) {
+                Log.w("GoogleSignIn", "Google sign in failed", e)
+            }
+        }
+    }
+
+
+    NavHost(
+        navController = navController,
+        startDestination = ChattrixScreens.LOGIN_SCREEN
+    ){
+
+    }
+
+}
