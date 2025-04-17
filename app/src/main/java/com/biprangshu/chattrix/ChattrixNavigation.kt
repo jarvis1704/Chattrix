f6d8fb27cd0e53343f63864baea253850b3d4a20
Biprangshu Das <dasbiprangshu@gmail.com>
2025-04-17 10:48:05 +0530

    Updated app navigation

--- app/src/main/java/com/biprangshu/chattrix/ChattrixNavigation.kt
+++ app/src/main/java/com/biprangshu/chattrix/ChattrixNavigation.kt
@@ -10,7 +10,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.biprangshu.chattrix.authentication.AuthViewModel
+import com.biprangshu.chattrix.home.HomeScreen
+import com.biprangshu.chattrix.onboarding.LoginScreen
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
 
@@ -38,7 +41,15 @@ fun ChattrixNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewMod
         navController = navController,
         startDestination = ChattrixScreens.LOGIN_SCREEN
     ){
+        composable(
+            route = ChattrixScreens.LOGIN_SCREEN
+        ) {
+            LoginScreen(navController = navController)
+        }
 
+        composable (route = ChattrixScreens.HOME_SCREEN){
+            HomeScreen()
+        }
     }
 
 }
